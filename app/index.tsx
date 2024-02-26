18214ed120ad80af4e0dfc8d41286643f4437b0e
Matias Rodriguez <matias.rodriguez@cliengo.com>
2024-02-25 22:26:32 -0300

    test board

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,125 @@
-import { View, Text } from "tamagui";
+import { View, Text, XStack, Button, YStack, Label } from 'tamagui';
 
 const Index = () => {
   return (
     <View>
-      <Text>Index Screen</Text>
+      <XStack>
+        <Label>General</Label>
+      </XStack>
+      <XStack gap={8}>
+        
+        <YStack gap={4} justifyContent="center">
+          <Button backgroundColor="lightblue" width={90}>
+            Juegos
+          </Button><Button backgroundColor="lightblue" width={90}>
+            1
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            2
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            3
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            4
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            5
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            6
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            Escalera
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            Full
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            Poker
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            Generala
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            Doble Generala
+          </Button>
+        </YStack>
+        <YStack gap={4} justifyContent="center">
+          <Button backgroundColor="lightblue" width={90}>
+            Player 1
+          </Button><Button backgroundColor="lightblue" width={90}>
+            1
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            2
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            3
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            4
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            5
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            6
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            20
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            30
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            40
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            x
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            x
+          </Button>
+        </YStack>
+        <YStack gap={4} justifyContent="center">
+          <Button backgroundColor="lightblue" width={90}>
+            Player 2
+          </Button><Button backgroundColor="lightblue" width={90}>
+            1
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            2
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            3
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            4
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            5
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            6
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            20
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            30
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            40
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            x
+          </Button>
+          <Button backgroundColor="lightblue" width={90}>
+            x
+          </Button>
+        </YStack>
+      </XStack>
     </View>
   );
 };
